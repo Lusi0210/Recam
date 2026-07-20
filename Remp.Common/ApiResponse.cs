999ffbdb9145a3dce2087dc030c673615671a82a
Lusi A <z5631341@ad.unsw.edu.au>
2026-07-20 23:47:40 +1000

    feat(RECAM-3): Add generic ApiResponse wrapper

---
+++ Remp.Common/ApiResponse.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Remp.Common;
+
+public class ApiResponse<T>
+{
+    public bool Success { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public T? Data { get; set; }
+    public List<string>? Errors { get; set; }
+
+    // Success
+    public static ApiResponse<T> SuccessResponse(T data, string message = "Success")
+    {
+        return new ApiResponse<T>
+        {
+            Success = true,
+            Message = message,
+            Data = data
+        };
+    }
+
+    // Failure
+    public static ApiResponse<T> FailureResponse(string message, List<string>? errors = null)
+    {
+        return new ApiResponse<T>
+        {
+            Success = false,
+            Message = message,
+            Errors = errors
+        };
+    }
+
+}
